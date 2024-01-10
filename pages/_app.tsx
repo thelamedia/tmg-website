7900b3d3c7b8d760ab2ffd78e4d33ed5a3df0090
thelamedia <51720178+thelamedia@users.noreply.github.com>
2024-01-10 10:54:33 -0600

    Scripts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,24 @@ import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import Script from "next/script";
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import {SpeedInsights} from "@vercel/speed-insights/next"
 
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <PlasmicRootProvider Head={Head}>
       <Script id="live-chat" src='//liveleads.us/scripts/invitation.ashx?company=thelamediagroup' async />
+      <Script id="gtm">
+        {` (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-5QZHLFKG');`}
+      </Script>
      <Component {...pageProps} />
+     <SpeedInsights />
     </PlasmicRootProvider>
   );
 }
